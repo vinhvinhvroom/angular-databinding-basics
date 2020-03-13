e9b8d6b2e1fcd001f4d1ed3366829a44e24f75c7
vinhvinhvroom <vinh.doan13@gmail.com>
2020-03-13 19:16:29 -0400

    ngIf for Data Conditional. Using directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!'
   serverName = 'Testserver';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -20,7 +21,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!'
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName 
   }
 
   onUpdateServerName(event: Event) {
